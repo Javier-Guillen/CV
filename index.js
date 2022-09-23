6b1ef9d700bd28d411b0003e95adc5bc7d2fa6d0
Javier-Guillen <javier.guillen.colomer@gmail.com>
2022-09-23 15:00:39 +0200

    Rutina

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 
 // document.querySelector("li .list")[0].innerHTML = "Javier Guillen"
 document.querySelector("ul li.list a").style.color = "red";
-console.log("");
+document.getElementById("click-me-button").style.backgroundColor = "yellow";
+document.querySelector("h1").classList.toggle("huge");
