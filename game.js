68a7477e1000cf91f807731560f847ebf234ae46
Javier-Guillen <javier.guillen.colomer@gmail.com>
2022-11-14 18:05:54 +0100

    -

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-alert("HOla")
+const buttonColors = [
+    "red",
+    "blue",
+    "green",
+    "yellow"    
+];
+
+const gamePattern = []
+
+for(let i = 0; i<4; i++){
+    nextSequence();
+    
+}
+
+
+function nextSequence() {
+    let randomNumber = (Math.floor(Math.random() * 4));
+    randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+}
